7da7cc8d0a37f774173fb72e5a97712a5cd48931
Mishalogika <byriamisha8@gmail.com>
2025-11-18 17:58:30 +0200

    Add files via upload
    code of programs

---
+++ gift(eng).py
@@ -0,0 +1,11 @@
+import webbrowser
+import time
+
+print("Hello! I have a gift for you...")
+time.sleep(2)
+print("Ready?")
+time.sleep(2)
+print("Open it now!")
+time.sleep(1)
+
+webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
---
+++ gift.py
@@ -0,0 +1,13 @@
+import webbrowser
+import time
+counter == 1
+print("Привет! У меня есть подарок для тебя...")
+time.sleep(2)
+print("Готов?")
+time.sleep(2)
+print("Открывай скорее!")
+time.sleep(1)
+while counter == 1:
+    webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+
+
